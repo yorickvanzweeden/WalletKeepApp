079f4b7deff94f29713949c4c8abc631b7139a77
PabloEnriqueIglesias <raoulschipper@outlook.com>
2018-01-14 18:44:50 +0100

    backbutton in assets now goes to home, textstyles now consistent

--- app/src/main/java/com/walletkeep/walletkeep/ui/asset/AssetActivity.java
+++ app/src/main/java/com/walletkeep/walletkeep/ui/asset/AssetActivity.java
@@ -264,4 +264,13 @@ public class AssetActivity extends AppCompatActivity {
         mSurfaceView.getHolder().unlockCanvasAndPost(canvas);
     }
 
+    // Force exit to Homescreen on backbutton press in asset activity
+    public void onBackPressed() {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
+
+    }
+
 }
